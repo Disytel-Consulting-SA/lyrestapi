cf56e4675c96e7bc97ee5751870632b34aaa6a7d
Federico Cristina <federico.cristina@disytel.net>
2023-05-31 11:26:00 -0300

    Unificacion en la salida de errores

--- src/main/java/org/libertya/api/controller/ErrorController.java
+++ src/main/java/org/libertya/api/controller/ErrorController.java
@@ -1,6 +1,7 @@
 package org.libertya.api.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,20 +11,11 @@ import java.util.HashMap;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
-    static HashMap<Integer, String> errorPages = new HashMap<>();
-
-    static {
-        errorPages.put(HttpStatus.FORBIDDEN.value(), "error-403");
-        errorPages.put(HttpStatus.NOT_FOUND.value(), "error-404");
-        errorPages.put(HttpStatus.INTERNAL_SERVER_ERROR.value(), "error-500");
-    }
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        if (status != null && errorPages.get(Integer.valueOf(status.toString())) != null) {
-            return errorPages.get(Integer.valueOf(status.toString()));
-        }
-        return "error";
+    public ResponseEntity<String> handleError(HttpServletRequest request) {
+        Integer code    =  request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) != null ? (Integer)request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) : 400;
+        String message  = request.getAttribute(RequestDispatcher.ERROR_MESSAGE) != null ? (String)request.getAttribute(RequestDispatcher.ERROR_MESSAGE) : "Solicitud incorrecta";
+        return new ResponseEntity<>(message, HttpStatus.valueOf(code));
     }
 }
