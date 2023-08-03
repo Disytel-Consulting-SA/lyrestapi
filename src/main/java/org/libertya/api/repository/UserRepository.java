40924b5231eed110814e2087dd7c5beb389502c0
Federico Cristina <federico.cristina@disytel.net>
2023-08-03 11:04:44 -0300

    Validar organizacion especificada en la gestion de token

--- src/main/java/org/libertya/api/repository/UserRepository.java
+++ src/main/java/org/libertya/api/repository/UserRepository.java
@@ -10,14 +10,16 @@ import java.util.Optional;
 public class UserRepository extends AbstractRepository {
 
     public Optional<PO> findUser(String username, String password, String clientId, String orgId) {
-        int[] ids = PO.getAllIDs(X_AD_User.Table_Name, String.format(" name = '%s' and password = '%s' and ad_client_id = %s and (ad_org_id = 0 or ad_org_id = %s)", username, password, clientId, orgId),  null);
+        int[] ids = PO.getAllIDs(
+                X_AD_User.Table_Name,
+                String.format(" name = '%s' and password = '%s' and ad_client_id = %s and (ad_org_id = %s or (ad_org_id = 0 and %s in (select ad_org_id from ad_org where ad_client_id = %s)))", username, password, clientId, orgId, orgId, clientId),
+                null);
         if (ids == null || ids.length == 0) {
             return Optional.empty();
         }
         try {
             return Optional.of(getPO(null, X_AD_User.Table_Name, new int[]{ids[0]}, null));
         } catch (Exception e) {
-            e.printStackTrace();
             return Optional.empty();
         }
     }
