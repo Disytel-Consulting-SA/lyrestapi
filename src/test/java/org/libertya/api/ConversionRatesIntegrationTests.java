c7dda8774ef390ace6cc277d5f1583e4efcd637c
fcristina77 <federico.cristina@disytel.net>
2025-07-23 07:55:18 -0300

    Generacion de conversion rate dummy en client 1000005 para tests de tipo companyMistMatch

--- src/test/java/org/libertya/api/ConversionRatesIntegrationTests.java
+++ src/test/java/org/libertya/api/ConversionRatesIntegrationTests.java
@@ -3,13 +3,22 @@ package org.libertya.api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.libertya.api.stub.model.ConversionRate;
+import org.openXpertya.util.DB;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 public class ConversionRatesIntegrationTests extends MaestroIntegrationTests{
+
+    protected static final int CONVRATE_OTHER_CLIENT_ID = 999999;
     @Override
     protected String getMaestroContent() throws JsonProcessingException {
+
+        // Conversion Rate de otra cia para pruebas (ignorar error al intentar insertar si ya existe)
+        DB.executeUpdate( "insert into c_conversion_rate " +
+                                " (c_conversion_rate_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, c_currency_id, c_currency_id_to, validfrom, validto, multiplyrate, dividerate, c_conversiontype_id) " +
+                                " values ("+ CONVRATE_OTHER_CLIENT_ID +", 1000005, 1000037, 'Y', now(), 100, now(), 100, 100, 118, '2001-01-01', '2099-01-01', 1, 1, 114)");
+
         ObjectMapper objectMapper = new ObjectMapper();
 
         ConversionRate conversionRate = new ConversionRate();
@@ -43,6 +52,6 @@ public class ConversionRatesIntegrationTests extends MaestroIntegrationTests{
 
     @Override
     protected String getRecordIDWithComapnyMismatch() {
-        return "-1"; // <-- No existen registros en la BD
+        return ""+CONVRATE_OTHER_CLIENT_ID;
     }
 }
